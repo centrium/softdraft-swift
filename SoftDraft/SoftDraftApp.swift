1f21ffa6eecbdce17a80af0bc19c58ddfea26f33
Matt Adams <matt.ic.adams@googlemail.com>
2026-01-20 19:15:57 +0000

    Initial Commit

---
+++ SoftDraft/SoftDraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftDraftApp.swift
+//  SoftDraft
+//
+//  Created by Matt Adams on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SoftDraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
